c8995eab55aa61373ab59c7c677ef58639cd65d5
kohei <kouhik06@gmail.com>
2017-07-23 12:48:44 +0900

    Ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:edit, :update, :destroy]
+
   def create
     @comment = current_user.comments.build(comment_params)
     @topic = @comment.topic
@@ -13,8 +15,33 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to topic_path(@topic)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    respond_to do |format|
+      if @comment.destroy
+        format.html {redirect_to topic_path(@topic), notice: 'コメントを削除しました。'}
+        format.js {render :index}
+      end
+    end
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:topic_id, :content)
+    end
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+      @topic = @comment.topic
     end
 end
